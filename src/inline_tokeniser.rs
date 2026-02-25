25d9e785eacc619a94d0f326b8f3be7541d901a0
Elliott <elliottjosephprice@gmail.com>
2026-02-25 23:08:55 +1100

    Used Logos for lexer

--- src/inline_tokeniser.rs
+++ src/inline_tokeniser.rs
@@ -1,34 +1,35 @@
 use crate::ast_types::Inline;
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+use logos::Logos;
+
+#[derive(Logos, Debug, PartialEq)]
 pub enum Tokens {
+    #[regex(r"[^\*\[\]\(\):]+", |lex| lex.slice().to_string())]
     TEXT(String),
+
+    #[token("*")]
     ASTERISK,
+
+    #[token("[")]
     LBRACKET,
+
+    #[token("]")]
     RBRACKET,
+
+    #[token("(")]
     LPAREN,
+
+    #[token(")")]
     RPAREN,
+
+    #[token(":")]
     COLON,
 }
-pub fn tokenise(content: &str) -> Vec<Tokens> {
-    let mut tokens: Vec<Tokens> = Vec::new();
 
-    for c in content.chars() {
-        match c {
-            '(' => tokens.push(Tokens::LPAREN),
-            ')' => tokens.push(Tokens::RPAREN),
-            '[' => tokens.push(Tokens::LBRACKET),
-            ']' => tokens.push(Tokens::RBRACKET),
-            ':' => tokens.push(Tokens::COLON),
-            '*' => tokens.push(Tokens::ASTERISK),
-            _ => match tokens.last_mut() {
-                Some(Tokens::TEXT(s)) => s.push(c),
-                _ => tokens.push(Tokens::TEXT(c.to_string())),
-            }
-        }
-    }
-    tokens
+pub fn tokenise(content: &str) -> Vec<Tokens> {
+    Tokens::lexer(content)
+        .filter_map(|result| result.ok())
+        .collect()
 }
 
 
@@ -39,6 +40,6 @@ mod tests {
     #[test]
     fn test_tokenise() {
         assert_eq!(tokenise(""), vec![]);
-        assert_eq!(tokenise("*Hi*"), vec![Tokens::ASTERISK, Tokens::TEXT("Hi".to_string()), Tokens::ASTERISK]);
+        assert_eq!(tokenise("*Hi12*"), vec![Tokens::ASTERISK, Tokens::TEXT("Hi12".to_string()), Tokens::ASTERISK]);
     }
 }
